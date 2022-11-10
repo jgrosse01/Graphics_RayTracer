457defe3fbf26fbad0f38bdfe09ddfca118498a3
Jake Grosse <jgrosse@carroll.edu>
2022-11-10 15:50:48 -0700

    ops: split HittableList into h and cpp, some refactoring

---
+++ src/rt_lib/HittableList.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Jake on 11/10/2022.
+//
+
+#include "HittableList.h"
+
+bool HittableList::hit(const Ray& r, double t_min, double t_max, hit_record& rec) const {
+    hit_record temp_rec;
+    bool hit_anything = false;
+    auto closest_so_far = t_max;
+
+    for (const auto& object : objects) {
+        if (object->hit(r, t_min, closest_so_far, temp_rec)) {
+            hit_anything = true;
+            closest_so_far = temp_rec.t;
+            rec = temp_rec;
+        }
+    }
+
+    return hit_anything;
+}
---
+++ src/rt_lib/HittableList.h
@@ -0,0 +1,30 @@
+/*
+ * Code directly adapted from implementation seen in "Ray Tracing in One Weekend" by Peter Shirley
+ */
+
+#ifndef GL_RAYTRACER_HIT_LIST_H
+#define GL_RAYTRACER_HIT_LIST_H
+
+#include "hittable.h"
+
+#include <memory>
+#include <vector>
+
+using std::shared_ptr;
+using std::make_shared;
+
+class HittableList : public hittable {
+public:
+    std::vector<shared_ptr<hittable>> objects;
+
+    HittableList() = default;
+
+    explicit HittableList(const shared_ptr<hittable>& object) { add(object); }
+
+    void clear() { objects.clear(); }
+    void add(const shared_ptr<hittable>& object) { objects.push_back(object); }
+
+    bool hit(const Ray& r, double t_min, double t_max, hit_record& rec) const override;
+};
+
+#endif //GL_RAYTRACER_HIT_LIST_H
