b3fa4627702ed5d64d630c338d0ce900bb76b731
Jake Grosse <jgrosse@carroll.edu>
2022-11-16 11:50:12 -0700

    feat: added separate file for random generation applications

--- src/rt_lib/RandomUtilites.cpp
+++ src/rt_lib/RandomUtilites.cpp
@@ -3,3 +3,39 @@
 //
 
 #include "RandomUtilites.h"
+
+double randomDouble() {
+    return s_distribution(s_generator);
+}
+
+double randomDouble(double min, double max) {
+    return min + (max-min)*randomDouble();
+}
+
+Vec3 randomVector() {
+    return {randomDouble(), randomDouble(), randomDouble()};
+}
+
+Vec3 randomVector(double min, double max) {
+    return {randomDouble(min, max), randomDouble(min, max), randomDouble(min, max)};
+}
+
+Vec3 randomVectorInUnitSphere() {
+    for(;;) {
+        auto p = randomVector(-1,1);
+        if (p.lengthSquared() > 1) continue;
+        return p;
+    }
+}
+
+Vec3 randomUnitVector() {
+    return unitVector(randomVectorInUnitSphere());
+}
+
+Vec3 randomVectorInHemisphere(const Vec3 &normalVector) {
+    Vec3 inUnitSphere = randomVectorInUnitSphere();
+    if (dot(inUnitSphere, normalVector) > 0.0) {
+        return inUnitSphere;
+    }
+    return -inUnitSphere;
+}
--- src/rt_lib/RandomUtilites.h
+++ src/rt_lib/RandomUtilites.h
@@ -5,4 +5,23 @@
 #ifndef GL_RAYTRACER_RANDOMUTILITES_H
 #define GL_RAYTRACER_RANDOMUTILITES_H
 
+#include <random>
+#include <cmath>
+#include "Vec3.h"
+
+using std::sqrt;
+using std::pow;
+
+static std::uniform_real_distribution<double> s_distribution(0.0, 1.0);
+static std::mt19937 s_generator;
+
+double randomDouble();
+double randomDouble(double min, double max);
+
+Vec3 randomVector();
+Vec3 randomVector(double min, double max);
+Vec3 randomVectorInUnitSphere();
+Vec3 randomUnitVector();
+Vec3 randomVectorInHemisphere(const Vec3 &normalVector);
+
 #endif //GL_RAYTRACER_RANDOMUTILITES_H
