df0f25ea9289f9e5d2e40485251de5ee3e3834f9
Jake Grosse <jgrosse@carroll.edu>
2022-11-16 20:14:31 -0700

    ops: now loads existing PPM instead of rendering new each time

--- main.cpp
+++ main.cpp
@@ -6,13 +6,31 @@ int g_pixelBorder = 50;
 
 PPM* g_ppm;
 
+
+//void reshape(int w, int h) {
+//    g_width= w;
+//    g_height = h;
+//    glMatrixMode(GL_PROJECTION);
+//    glLoadIdentity();
+//    glViewport(0, 0, g_width, g_height);
+//    glOrtho(0, g_width+2*g_pixelBorder, 0, g_height+2*g_pixelBorder, -1, 1);
+//    glutPostRedisplay();
+//}
+
 void display() {
+    //reshape(g_width+2*g_pixelBorder, g_height+2*g_pixelBorder);
+
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_POINTS);
 
     for (int j = g_height-1; j >= 0; j--) {
         for (int i = 0; i < g_width; i++) {
-            glColor3d(g_ppm->at(j, i)[0], g_ppm->at(j,i)[1], g_ppm->at(j,i)[2]);
+            double r, g, b;
+            r = g_ppm->at(j,i)[0];
+            g = g_ppm->at(j,i)[1];
+            b = g_ppm->at(j,i)[2];
+            //std::cout << r << " " << g << " " << b  << std::endl;
+            glColor3d(r, g, b);
             glVertex2i(i + g_pixelBorder,j + g_pixelBorder);
         }
     }
@@ -28,13 +46,16 @@ void init() {
     glOrtho(0, g_width+2*g_pixelBorder, 0, g_height+2*g_pixelBorder, -1, 1);
 }
 
-void reshape(int w, int h) {
+void keys() {
 
 }
 
 int main(int argc, char** argv) {
-    g_ppm = renderScene(g_width, g_height);
-    g_ppm->savePPM("raytrace");
+    g_ppm = PPM::loadPPM("raytrace");
+    g_width = g_ppm->width();
+    g_height = g_ppm->height();
+    //g_ppm = renderScene(g_width, g_height);
+    //g_ppm->savePPM("raytrace");
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
@@ -45,6 +66,7 @@ int main(int argc, char** argv) {
     init();
 
     glutDisplayFunc(display);
+    //glutReshapeFunc(reshape);
 
     glutMainLoop();
 
