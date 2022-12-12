4072436441b85c3cf64148314d916220627a80c7
jgrosse01 <jgrosse@carroll.edu>
2022-12-12 14:26:26 -0700

    feat: Dielectrics work properly; fix: schlick's approx. of refractance

--- src/materials/Dielectric.h
+++ src/materials/Dielectric.h
@@ -5,12 +5,23 @@
 #ifndef GL_RAYTRACER_DIELECTRICS_H
 #define GL_RAYTRACER_DIELECTRICS_H
 
+#include "MaterialInterface.h"
+#include "../rt_lib/hittable.h"
+#include "../rt_lib/RandomUtilites.h"
 
 
-class Dielectrics {
+class Dielectric : public MaterialInterface {
+private:
+    double ir;
+    Color color;
 
+    static double reflectance(double cosine, double reflectanceIndex);
+
+public:
+    explicit Dielectric(double indexOfRefraction, const Color& color);
+
+    bool scatter(const Ray& rayIn, const hit_record& record, Color& attenuation, Ray& Scattered) const override;
 };
-
 
 
 #endif //GL_RAYTRACER_DIELECTRICS_H
