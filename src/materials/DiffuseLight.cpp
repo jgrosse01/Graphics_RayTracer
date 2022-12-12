cea0a1b5cf2fcd0476948977c2e896fea133d055
jgrosse01 <jgrosse@carroll.edu>
2022-12-12 15:15:50 -0700

    feat: implemented diffuse light objects

--- src/materials/DiffuseLight.cpp
+++ src/materials/DiffuseLight.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "DiffuseLight.h"
+
+DiffuseLight::DiffuseLight(shared_ptr<TextureInterface> a) {
+    this->emit_ = a;
+}
+
+DiffuseLight::DiffuseLight(Color color) {
+    this->emit_ = make_shared<SolidColor>(color);
+}
+
+bool DiffuseLight::scatter(const Ray &rayIn, const HitRecord &record, Color &attenuation, Ray &scattered) const {
+    return false;
+}
+
+Color DiffuseLight::emitted(double u, double v, const Point3 &p) const {
+    return emit_->value(u,v,p);
+}
--- src/materials/DiffuseLight.h
+++ src/materials/DiffuseLight.h
@@ -5,15 +5,27 @@
 #ifndef GL_RAYTRACER_DIFFUSELIGHT_H
 #define GL_RAYTRACER_DIFFUSELIGHT_H
 
+#include <memory>
 #include "MaterialInterface.h"
+#include "../rt_lib/RandomUtilites.h"
+#include "../textures/TextureInterface.h"
+#include "../textures/SolidColor.h"
+
+using std::shared_ptr;
+using std::make_shared;
 
 
 class DiffuseLight : public MaterialInterface{
+private:
+    shared_ptr<TextureInterface> emit_;
+
 public:
-    DiffuseLight(Color color) {
+    DiffuseLight(shared_ptr<TextureInterface> a);
+    DiffuseLight(Color color);
 
-    }
+    virtual bool scatter(const Ray& rayIn, const HitRecord& record, Color& attenuation, Ray& scattered) const override;
 
+    virtual Color emitted(double u, double v, const Point3& p) const override;
 };
 
 
